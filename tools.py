14ab7abbeb61d37fc9def254ec0b704d6e87b6df
Gustavo A Salazar Gomez <gustavo.salazar@uao.edu.co>
2022-04-21 15:59:10 -0500

    Finished Compute Attitude

--- tools.py
+++ tools.py
@@ -1,5 +1,39 @@
 import numpy as np
 import math
+from scipy.linalg import svd
+
+def quaternion2euler(quat):
+
+    euler = np.zeros((3,1))
+
+    q0 = quat[0]
+    q1 = quat[1]
+    q2 = quat[2]
+    q3 = quat[3]
+    euler[2] = math.atan2( (2 * q1 * q2 + 2 * q0 * q3) , (2 * q0 * q0 + 2 * q1 * q1 - 1))
+    euler[1] = math.asin(-2*q1*q3 + 2*q0*q2)
+    euler[0] = math.atan2( (2 * q2 * q3 + 2 * q0 * q1) , (2 * q0 * q0 + 2 * q3 * q3 - 1))
+
+    return euler
+
+
+def quaternionmul(p,q):
+    
+    p0 = p[0]
+    p1 = p[1]
+    p2 = p[2]
+    p3 = p[3]
+
+    M = np.vstack( [p0 , -p1 , -p2 , -p3], 
+                   [p1 , p0 , -p3 , p2 ],
+                   [p2 , p3 , p0, -p1 ],
+                   [p3 , -p2, p1 , p0])
+
+    v = np.vstack(q[0] , q[1] , q[2]  ,q[3])
+
+    r = M * v
+
+    return r
 
 def dcm2quaternion(C):
     f = np.zeros((4,1))
@@ -189,3 +223,86 @@ def Compute_Attitude(yg,ya,ym,tt,Rg,Ra,Rm):
         uk = uk / M1    
         
         fooR2 = H1*P*np.transpose(H1) + Ra
+
+        u,s,v = svd(uk)
+        u1 = u[:,0]
+        u2 = u[:,1]
+        u3 = u[:,2]
+
+        lambda_ = [s[0], s[1], s[2]]
+
+        mu =  [ np.transpose(u1) * fooR2 * u1 , 
+                np.transpose(u2) * fooR2 * u2 , 
+                np.transpose(u3) * fooR2 * u3]
+
+        if max(lambda_ - mu) > gamma :
+            r2count = 0
+            Qstar = max(lambda_[0]-mu[0],0)*u1*np.transpose(u1) \
+                + max(lambda_[1] -mu[1],0)*u2*np.transpose(u2)\
+                + max(lambda_[2]-mu[2],0)*u3*np.transpose(u3)
+        else:
+            r2count = r2count + 1
+            if  r2count < M2 :
+                Qstar = max(lambda_[0]-mu[0],0)*u1*np.transpose(u1)\
+                 + max(lambda_[1]-mu[1],0)*u2*np.transpose(u2) \
+                 + max(lambda_[2]-mu[2],0)*u3*np.transpose(u3)
+            else:
+                Qstar = np.zeros((3,3))
+      
+        K_a = P_p*np.transpose(H1)/(H1*P_p*np.transpose(H1) + Ra + Qstar)        
+
+        xhat_a =  x + K_a*(z1 - H1*x)        
+
+        P_a = (np.eye((9,9)) - K_a*H1)*P_p*np.transpose(np.eye((9,9))- K_a*H1)\
+               + K_a*(Ra + Qstar)*np.transpose(K_a)
+
+        
+        qe = np.vstack(1,xhat_a[0:2])
+        
+        q4[:,i] = quaternionmul(q4[:,i],qe)
+        
+        q4[:,i] = q4[:,i]/np.linalg.norm(q4[:,i])
+        
+        xhat_a[0:2] = np.zeros((3,1))
+
+        Cq = quaternion2dcm(q4[:,i])
+        
+        H_m = [2*(vec2product(Cq*m_tild)), np.zeros((3,3)),np.zeros((3,3))]
+        
+        z_m = ym[:,i-1] - Cq*m_tild
+        
+        P_m =  np.vstack(np.hstack(P_a[0:2,0.2], np.zeros((3,6))),
+                         np.hstack(np.zeros((6,3)), np.zeros((6,6))))
+        
+        r3 = Cq * np.vstack(0,0,1)
+
+
+        K_m = np.vstack(np.hstack(r3*np.transpose(r3), np.zeros((3,6))), 
+                        np.hstack(np.zeros((6,3)), np.zeros((6,6))))\
+              *P_m*np.transpose(H_m)/(H_m*P_m*np.transpose(H_m)+Rm)
+
+        x = xhat_a + K_m*(z_m-H_m*xhat_a)
+        
+
+        P = (np.eye((9,9))-K_m*H_m)*P_a*np.transpose(np.eye((9,9))-K_m*H_m)\
+            +K_m*Rm*np.transpose(K_m)
+        
+        bghat = bghat + x[3:5]
+        x[3:5] = np.zeros((3,1))
+
+        bahat = bahat + x[6:8]
+        x[6:8] = np.zeros((3,1))
+
+        qe = np.vstack(1,xhat_a[0:2])
+        
+        q4[:,i] = quaternionmul(q4[:,i],qe)
+        
+        q4[:,i] = q4[:,i]/np.linalg.norm(q4[:,i])
+        
+        Cq = quaternion2dcm(q4[:,i])
+        
+        eulercom4[:,i]=quaternion2euler(q4[:,i])
+
+    return q4, eulercom4, bahat, bghat
+
+   
